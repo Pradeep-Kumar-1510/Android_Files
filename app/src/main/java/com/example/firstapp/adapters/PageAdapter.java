41f634af96ed872a097ac933754a0d1457a125e6
Pradeep Raja <pradeep.raja@aspiresys.com>
2024-03-06 17:50:32 +0530

    Implemented Foreground Service with threads

--- app/src/main/java/com/example/firstapp/adapters/PageAdapter.java
+++ app/src/main/java/com/example/firstapp/adapters/PageAdapter.java
@@ -8,6 +8,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import com.example.firstapp.fragments.JokeApiFragment;
 import com.example.firstapp.fragments.MapFragment;
 import com.example.firstapp.fragments.PostApiFragment;
+import com.example.firstapp.fragments.ThreadFragment;
 
 public class PageAdapter extends FragmentStateAdapter {
     public PageAdapter(@NonNull FragmentActivity fragmentActivity) {
@@ -21,13 +22,16 @@ public class PageAdapter extends FragmentStateAdapter {
             return new PostApiFragment();
         } else if (position == 1) {
             return new MapFragment();
-        } else if (position == 2)
+        } else if (position == 2) {
             return new JokeApiFragment();
+        } else if (position == 3) {
+            return new ThreadFragment();
+        }
         return new PostApiFragment();
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        return 4;
     }
 }
